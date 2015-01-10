a4bbf35762d1d9142dec32d9d6bf2201dcd6b6bf
Payam Azadiprivate <azadireborn@live.com>
2015-01-10 13:39:09 -0500

    Initial commit

---
+++ mount-helper.bat
@@ -0,0 +1,1 @@
+start mount.bat /B
---
+++ mount.bat
@@ -0,0 +1,4 @@
+ping 1.1.1.1 -n 1 -w 20000 > nul
+net use {share} /USER:{username} {password}
+start "" "{additional startup program. Such as a media server software..}" /B
+exit
